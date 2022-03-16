e5e41a83df60d2d5d6935e94acff7b33d1ca5219
wwy <wang_weiyi@foxmail.com>
2022-03-16 20:21:07 +0800

    finish the interface

--- src/main/scala/bus/ToCfi.scala
+++ src/main/scala/bus/ToCfi.scala
@@ -1,8 +1,45 @@
-//package bus
-//
-//import chisel3._
-//import chisel3.util._
-//
-//class Cfi2UartConverter extends Module {
-//
-//}
+package bus
+
+import chisel3._
+import chisel3.util._
+
+class UartTxPathIO extends Bundle {
+  val txData = DecoupledIO(Output(UInt(8.W)))
+}
+
+class UartRxPathIO extends Bundle {
+  val rxData = DecoupledIO(Input(UInt(8.W)))
+}
+
+class CfiTo extends Bundle {
+  val valid = Input(Bool())
+  val id = Input(UInt(2.W))
+  val cmd = Input(UInt(3.W))
+  val prepresrc = Input(UInt(32.W))
+  val presrc = Input(UInt(32.W))
+  val src = Input(UInt(32.W))
+  val dst = Input(UInt(32.W))
+}
+
+class ToCfi extends Bundle {
+  val valid = Output(Bool())
+  val id = Output(UInt(2.W))
+  val cmd = Output(UInt(3.W))
+  val srcAddr = Output(UInt(32.W))
+  val dstAddr = Output(UInt(32.W))
+}
+
+class Cfi2UartConverter extends Module {
+  val io = IO(new Bundle {
+    val in = new CfiTo
+    val out = new UartTxPathIO
+  })
+
+}
+
+class Uart2CfiConverter extends Module {
+  val io = IO(new Bundle {
+    val in = new UartRxPathIO
+    val out = new ToCfi
+  })
+}
